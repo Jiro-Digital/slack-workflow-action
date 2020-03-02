1914053449f35010bf55b12bfe90e025411f9e32
Sam Alexander <samalexander@users.noreply.github.com>
2020-03-02 16:11:02 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const core = require('@actions/core');
+const axios = require('axios');
+const fs = require('fs');
+const { join } = require('path');
+
+async function run() {
+  try {
+    const path = core.getInput('path');
+    const environment = core.getInput('environment');
+    const webhookUrl = core.getInput('webhookUrl');
+
+    const pJson = fs.readFileSync(join(path, 'package.json'));
+    const { name, version } = JSON.parse(pJson);
+
+    const { statusText } = await axios.post(webhookUrl, {
+      message: `${name} v${version} deployed to ${environment}`
+    });
+
+    core.setOutput('status', statusText);
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
